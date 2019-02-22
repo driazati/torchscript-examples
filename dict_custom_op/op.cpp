d7950e362efddcd230af10d6daeb5dacb6cbd8d2
davidriazati <davidriazati@fb.com>
2019-02-22 15:42:56 -0800

    use custom op bindings

--- dict_custom_op/op.cpp
+++ dict_custom_op/op.cpp
@@ -1,34 +1,11 @@
 #include <torch/script.h>
 #include <vector>
 
-// A std::unordered_map behind the scenes
-using JitDict = c10::ivalue::DictUnorderedMap<torch::jit::IValue, torch::jit::IValue>;
 
-// Register an operator with a custom schema
-torch::jit::RegisterOperators registry({
-  torch::jit::Operator(
-    // Schema should be something like:
-    //    namespace::op_name(argument types) -> return type
-    // Dictionaries of the form `Dict(key type, value type)`
-    "my_ops::dictionary_op(str key, Dict(str, Tensor) dict) -> Tensor",
-    [](torch::jit::Stack &stack) {
-      JitDict dict = torch::jit::pop(stack).toGenericDictRef();
-      std::string key = torch::jit::pop(stack).toStringRef();
+torch::Tensor get_tensor(std::unordered_map<std::string, torch::Tensor> map,
+                         std::string key) {
+  return map.find(key)->second;
+}
 
-      // Do some stuff with the map
-      std::cout << "I'm a custom operator and this is my favorite dictionary:\n";
-      for (auto item : dict) {
-        std::cout << "\t" << item.first << ":\t" << item.second << "\n";
-      }
-
-      auto item = dict.find(key);
-      if (item == dict.end()) {
-        std::cout << "Key " << key << " not found!\n";
-        torch::jit::push(stack, torch::zeros({2}));
-        return 0;
-      }
-
-      torch::jit::push(stack, item->second);
-      return 0;
-    })
-});
+static auto registry =
+  torch::jit::RegisterOperators("my_ops::get_tensor", &get_tensor);
--- dict_custom_op/use_custom_op.py
+++ dict_custom_op/use_custom_op.py
@@ -1,16 +1,22 @@
 import torch
 
 # Load in custom operator
-lib_path = "/home/davidriazati/pytorch3/torchscript-examples/dict_custom_op/build/libdictionary_op.so"
+lib_path = "./build/libdictionary_op.so"
 torch.ops.load_library(lib_path)
+
 
 # Use in TorchScript
 @torch.jit.script
-def fn(key, my_dict):
-    # type: (str, Dict[str, Tensor]) -> Tensor
-    return torch.ops.my_ops.dictionary_op(key, my_dict)
+def lookup_all(keys, my_dict):
+    # type: (List[str], Dict[str, Tensor]) -> List[Tensor]
+    output = []
 
-print(fn.graph)
+    for key in keys:
+        output.append(torch.ops.my_ops.get_tensor(my_dict, key))
 
-a_dict = {'goodbye': torch.ones(2), 'hello': torch.ones(2) + 1}
-print(fn('hello', a_dict))
+    return output
+
+
+keys = ['a', 'b', 'c']
+a_dict = {char: torch.ones(1) + ord(char) - ord("a") for char in "abcdefg"}
+print(lookup_all(keys, a_dict))  # [tensor([1.]), tensor([2.]), tensor([3.])]
